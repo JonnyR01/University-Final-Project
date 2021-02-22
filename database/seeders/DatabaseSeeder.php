f7fc51fa6b698116c7250d711de16c47dd0ad013
Jonathan <rainey-j6@ulster.ac.uk>
2021-02-22 15:51:08 +0000

    Created roles, roleuser and userdata

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ProductsSeeder::class
         ]);
+        $this->call(UserSeeder::class);
+        $this->call(RoleSeeder::class);
+        $this->call(RoleUserSeeder::class);
     }
 }
